1a88f06f31f5479c07e06443104eda4d5cffb705
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-24 23:24:47 +0200

    add image preview

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -7,10 +7,28 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
   permalink: string = ''; // for two-way-binding you need a global variable.
+  imgSrc: any = './assets/placeholder-img.jpg';
+  selectedImg: any;
 
+
+  // Show Permalink once user starts typing
   onTitleChanged(event: any)  {
     const title = event.target.value;
     this.permalink = title.toLowerCase().replace(/\s+/g, '-');
+  }
+
+
+  // Show Image Preview
+  showPreview(event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgSrc = e.target?.result;
+    }
+
+    reader.readAsDataURL(event.target.files[0]);
+
+    // Assign image-selection to a global variable
+    this.selectedImg = event.target.files[0];
   }
 
 }

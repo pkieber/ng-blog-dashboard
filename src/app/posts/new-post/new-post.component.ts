d7602de2e48d21dc7a3ec702a80595d597fe06a6
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-24 23:09:22 +0200

    add permalink-input

--- src/app/posts/new-post/new-post.component.ts
+++ src/app/posts/new-post/new-post.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.scss']
 })
 export class NewPostComponent {
+  permalink: string = ''; // for two-way-binding you need a global variable.
+
+  onTitleChanged(event: any)  {
+    const title = event.target.value;
+    this.permalink = title.toLowerCase().replace(/\s+/g, '-');
+  }
 
 }

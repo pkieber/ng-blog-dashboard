1388375b70c377cb3e7aac9a0079ce70ce80e40e
pkieber <113725311+pkieber@users.noreply.github.com>
2023-09-28 15:03:14 +0200

    add logout-btn + save current user in local storage

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  userEmail: string | null;
+
+  constructor(private authService: AuthService) {
+    // Retrieve the user from local storage and parse it as a JavaScript object
+    const userString = localStorage.getItem('user');
+    const userObject = userString ? JSON.parse(userString) : null;
+
+    // Extract the email property
+    this.userEmail = userObject ? userObject.email : null;
+  }
+
+  onLogout() {
+    this.authService.logout();
+
+    // Remove the user from local storage
+    localStorage.removeItem('user');
+    this.userEmail = null;
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, signInWithEmailAndPassword, User } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  currentUser: User | null = null;
 
-  constructor(private auth: Auth, private toastr: ToastrService, private router: Router ) { }
-
+  constructor(private auth: Auth, private toastr: ToastrService, private router: Router) {
+    this.loadUser(); // Initialize currentUser when the service is created
+  }
 
   login(email: string, password: string) {
     return signInWithEmailAndPassword(this.auth, email, password)
@@ -23,4 +24,22 @@ export class AuthService {
       });
   }
 
+  private loadUser() {
+    this.auth.onAuthStateChanged((user) => {
+      this.currentUser = user;
+      localStorage.setItem('user', JSON.stringify(user)); //
+    });
+  }
+
+
+  logout() {
+    this.auth.signOut().then(() => {
+      this.currentUser = null;
+      this.toastr.success('Logged out successfully', 'Success');
+      this.router.navigate(['/login']);
+    }).catch(() => {
+      this.toastr.error('Logout failed', 'Error');
+    });
+  }
 }
+
